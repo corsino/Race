1f5f1686154121f73e27f5d50ea4e6838496cb91
Superbank <ms.partner@supermeios.com.br>
2019-08-19 17:17:20 -0300

    Swagger configuration

--- Gyp.Corrida.API/Startup.cs
+++ Gyp.Corrida.API/Startup.cs
@@ -2,47 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Gyp.Corrida.InfraStructure.Bootstrap;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Gyp.Corrida.API
 {
     public class Startup
     {
+        private ApplicationStartup _startup;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            _startup = new ApplicationStartup();
         }
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+             _startup.ConfigureServices(services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseMvc();
+           _startup.Configure(app, env);
         }
     }
 }
---
+++ Gyp.Corrida.InfraStructure.Bootstrap/ApplicationStartup.cs
@@ -0,0 +1,36 @@
+﻿using Gyp.Corrida.InfraStructure.Bootstrap.Extensions;
+using Gyp.Corrida.InfraStructure.Bootstrap.ServiceCollection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyp.Corrida.InfraStructure.Bootstrap
+{
+    public class ApplicationStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+           
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            });
+
+            services.AddGypCorridaSwagger();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+           app.UseGypCorridaSwagger();
+            app.UseMvc();
+        }
+    }
+}
